9553be5613cf927e8f0a2444c260bbf8164bc693
hahahafree <875298955@qq.com>
2018-10-27 22:17:42 +0800

    section

---
+++ 005_高级特性/005_1 切片/section_2.py
@@ -0,0 +1,28 @@
+#!/bin/usr/env python3
+# -*- coding:utf-8 -*-
+
+def trim(s):
+    if s == '':
+        return s
+    if s[0] == ' ':
+        return trim(s[1:])
+    elif s[-1] == ' ':
+        return trim(s[:-2])
+    else:
+        return s
+
+# 测试:
+if trim('hello  ') != 'hello':
+    print('测试失败!')
+elif trim('  hello') != 'hello':
+    print('测试失败!')
+elif trim('  hello  ') != 'hello':
+    print('测试失败!')
+elif trim('  hello  world  ') != 'hello  world':
+    print('测试失败!')
+elif trim('') != '':
+    print('测试失败!')
+elif trim('    ') != '':
+    print('测试失败!')
+else:
+    print('测试成功!')
